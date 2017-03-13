080c211a589c4bbc90a59ee27b846ba426a1984c
EklektikArtist <infoforyou42@gmail.com>
2017-03-13 14:37:47 -0500

    Working Highlight and Countries with ID

---
+++ DosMaps/test.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("#highlight").click(function(event){
+		var myElement = document.querySelector("#superman");
+		myElement.style.backgroundColor = "#D93600";
+	})
+})
